307741c2e6e992e81daf4f91fdaa82b215ff2e94
Rodrigo Barreto <rodrigo.barreto@qi.edu.br>
2020-10-07 12:02:59 -0300

    Pokemons: criação da view

--- index.php
+++ index.php
@@ -18,35 +18,65 @@
 </head>
 
 <body>
+
+	<?php
+	include "pokemon.php";
+	include "pokemonview.php";
+
+	$p1 = new Pokemon(
+		"Arceus",
+		"Simplesmente o criador de tudo, Arceus é como um Deus no universo de Pokémon. Assim que ele nasceu, Arceus criou tudo a sua volta, como tempo e espaço, e deixou o “Trio da Criação” encarregado de cuidar disso. Curiosamente havia uma forma de capturar Arceus planejada para Pokémon Diamond & Pearl, porém a Nintendo nunca a lançou por achá-la complicada demais e apenas o distribuiu em raras ocasiões em eventos especiais.",
+		"arceus.jpg",
+		1500,
+		2000
+	);
+
+	$p2 = new Pokemon(
+		"Celebi",
+		"Inicialmente considerado como “O Mew da geração Gold & Silver” pois só podia ser obtido em eventos, Celebi provou-se um tipo diferente de pokémon lendário, um pouco mais simples em objetivo porém ainda poderoso. Ele é um guardião de florestas que procura sempre protegê-las e para isso pode até mesmo viajar no tempo, o que parece um pouco exagerado.",
+		"celebi.jpg",
+		190,
+		120
+	);
+
+	$p3 = new Pokemon(
+		"Heatran",
+		"Ao considerar a importância de Heatran, é estranho imaginar o quanto ele fica em segundo plano em Pokémon Diamond & Pearl. Ele representa o núcleo do planeta Terra e é tão velho quanto o “Trio da Criação”, porém simplesmente não ganhou muito destaque nem nos jogos nem na série animada. Ele possui uma particularidade no entanto, já que é o único pokémon lendário que pode ser encontrado em diferentes gêneros, macho ou fêmea.",
+		"heatran.jpg",
+		250,
+		80
+	);
+
+
+	?>
+
 	<div class="container">
+		<div class="row text-center" id="titulo">
+			<div class="col-md-12">
+				<h1>Pokemons Lendários</h1>
+			</div>
+		</div>	
+
 
 		<div class="row text-center">
-			<div class="col-md-12">
 
-				<?php
-				include "pokemon.php";
+			<?php
 
-				$p1 = new Pokemon(
-					"Arceus",
-					"Simplesmente o criador de tudo, Arceus é como um Deus no universo de Pokémon. Assim que ele nasceu, Arceus criou tudo a sua volta, como tempo e espaço, e deixou o “Trio da Criação” encarregado de cuidar disso. Curiosamente havia uma forma de capturar Arceus planejada para Pokémon Diamond & Pearl, porém a Nintendo nunca a lançou por achá-la complicada demais e apenas o distribuiu em raras ocasiões em eventos especiais.",
-					"arceus.jpg",
-					250,
-					50
-				);
-
-				$p1->setNome("Alceu");
-				$p1->setAtaque(350);
-
-				echo "<img src=img/" . $p1->getFoto() . "><br>";
-				echo $p1->__toString();
-				
-				?>
+			$pkView = new PokemonView();
+			$pkView->gerarCard($p1);
+			$pkView->gerarCard($p2);
+			$pkView->gerarCard($p3);
+			$pkView->gerarCard($p1);
+			$pkView->gerarCard($p2);
+			$pkView->gerarCard($p3);
 
 
-			</div>
+
+
+			?>
+
+
 		</div>
-
-
 	</div>
 </body>
 
---
+++ pokemonview.php
@@ -0,0 +1,36 @@
+<?php
+
+    class PokemonView{
+
+        public function gerarCard($pokemon){
+            $nomePokemon = $pokemon->getNome();
+            $descricaoPokemon = $pokemon->getDescricao();
+            $fotoPokemon = $pokemon->getFoto();
+            $ataquePokemon = $pokemon->getAtaque();
+            $defesaPokemon = $pokemon->getDefesa();
+           
+            echo "
+            <div class='col-md-4' style='margin-top: 20px;'>
+                <div class='img-thumbnail' style='background-color: rgba(0,0,0,0.9); color: white;'>
+                    <img src='img/$fotoPokemon' style='width: 100%; height: 100%;'>
+                    <div class='caption'>
+                        <br>
+                        <h2 style='font-size: 1.5em;'>$nomePokemon</h2>
+                        <br>
+                        <p style='font-size:0.8em; text-align: justify; height: 150px;'>$descricaoPokemon</p>
+                        <br>
+                        <p style='color: deeppink; '>Pontos de ataque: $ataquePokemon</p>
+                        <p style='color: gold; '>Pontos de defesa: $defesaPokemon</p>
+                    </div>
+                </div>
+            </div>
+        
+           ";
+
+        }
+
+
+    }
+
+
+?>
